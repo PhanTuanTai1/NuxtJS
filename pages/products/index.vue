34ea31f6fc8acd7fa742b531159717888ec24716
phantuantai1234@gmail.com <phantuantai1234@gmail.com>
2020-11-28 22:05:06 +0700

    save assgiment

---
+++ pages/products/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <nuxt-link to="/products/1">Products 1</nuxt-link><br/>
+        <nuxt-link to="/products/2">Products 2</nuxt-link>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
